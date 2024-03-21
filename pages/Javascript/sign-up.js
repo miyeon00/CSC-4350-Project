24041dd430a333d1ae97204906b49e90c0f54920
miyeon00 <miyeonp93@gmail.com>
2024-03-21 16:14:38 -0400

    Update sign-up.js
    sign-up

--- pages/Javascript/sign-up.js
+++ pages/Javascript/sign-up.js
@@ -1,31 +1,33 @@
 function Register(){
 
-    var pantherId = document.getElementById("pantherId").value; // 9 digits
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
-    var firstName = document.getElementById("firstName").value;
-    var lastName = document.getElementById("lastName").value;
-    var email = document.getElementById("email").value;
-    var role = document.querySelector('input[name="role"]:checked').value;
+  var pantherId = document.getElementById("pantherId").value; // 9 digits
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+  var firstName = document.getElementById("firstName").value;
+  var lastName = document.getElementById("lastName").value;
+  var email = document.getElementById("email").value;
+  var role = document.querySelector('input[name="role"]:checked').value;
 
-    var params = {
-      pantherId: pantherId,
-      username: username,
-      password: password,
-      firstName: firstName,
-      lastName: lastName,
-      email: email,
-      role:role
-    }
+  var params = {
+    pantherId: pantherId,
+    username: username,
+    password: password,
+    firstName: firstName,
+    lastName: lastName,
+    email: email,
+    role:role
+  }
 
-    console.log(params)
+  console.log(params)
+
+  var user_create_url = common_url+"/users/create";  
   
-    var user_create_url = common_url+"/users/create";  
-    
-    postData(user_create_url, params).then((data) => {
-      console.log(data); // JSON data parsed by `data.json()` call
-    });
-    
+  postData(user_create_url, params).then(data => {
+    let message  = data.message;
+    console.log(message);      
+    alert(message);
+
+});
 
 }
 
